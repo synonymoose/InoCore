cc05b9e2d86bffa274646752d64717a16eba9551
cooler-SAI <cooler19860212@yahoo.com>
2017-09-08 21:16:00 +0300

    DB: errors fixed

---
+++ sql/updates/world/2017_09_08_world_07.sql
@@ -0,0 +1,16 @@
+UPDATE `creature_template` SET `ScriptName` = 'npc_merchant_lorvo' WHERE `entry` = 54943;
+UPDATE `creature_template` SET `ScriptName` = 'mob_huojin_monk' WHERE `entry` = 60176;
+UPDATE `creature_template` SET `ScriptName` = 'mob_brewer_lin' WHERE `entry` = 60253;
+UPDATE `creature_template` SET `ScriptName` = 'mob_jaomin_ro' WHERE `entry` = 54611;
+UPDATE `creature_template` SET `ScriptName` = 'mob_huojin_trainee' WHERE `entry` = 54586;
+UPDATE `creature_template` SET `ScriptName` = 'mob_master_shang_xi' WHERE `entry` = 53566;
+UPDATE `creature_template` SET `ScriptName` = 'npc_wrathion_mason' WHERE `entry` = 64822;
+UPDATE `creature_template` SET `ScriptName` = 'mob_the_wrestler' WHERE `entry` = 60997;
+
+
+
+
+
+
+
+
