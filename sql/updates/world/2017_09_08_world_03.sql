6470e8d2a017bafc53cd2cee00bb0635dbbf0fce
Alessandro Kulya <K.u.l.y.a@mail.ru>
2017-09-08 13:41:20 +0300

    SQL Update

--- sql/updates/world/2017_09_08_world_03.sql
+++ sql/updates/world/2017_09_08_world_03.sql
@@ -1,7 +1,7 @@
 SET @SI_ENTRY = 49869;
 SET @BBW_ENTRY = 49871;
+SET @SIS_ENTRY = 50047;
 
-UPDATE `creature_template` SET `scriptname`="npc_blackrock_battle_worg" WHERE `entry`=@BBW_ENTRY;
 DELETE FROM `creature_text` WHERE `entry` IN (@SI_ENTRY);
 INSERT INTO `creature_text` (`entry`, `groupid`, `id`, `text`, `type`, `language`, `probability`, `emote`, `duration`, `sound`, `comment`) VALUES
 (@SI_ENTRY, 1, 0, 'Come, monsters! We will crush you!', 14, 0, 15, 0, 0, 0, 'Stormwind Infantry - Yell'),
@@ -17,3 +17,15 @@ INSERT INTO `locales_creature_text` (`entry`, `textGroup`, `id`, `text_loc1`, `t
 (@SI_ENTRY, 1, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Будьте начеку! Поблизости орки и ворги!', NULL, NULL),
 (@SI_ENTRY, 1, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Пора вас отправить вслед за вашими грязными псами!', NULL, NULL),
 (@SI_ENTRY, 1, 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'Ваши ворги бессильны перед военной мощью Штормграда!', NULL, NULL);
+
+UPDATE `creature` SET `MovementType`=1,`unit_flags`=770 WHERE `guid` IN (18270, 18267, 18263, 18281, 18277, 18261, 18262, 18211, 18202, 18193, 18180, 18169, 18154, 18156, 18152);
+UPDATE `creature_template` SET `AIName`="" WHERE `entry`=@SIS_ENTRY;
+UPDATE `creature` SET `curhealth`=1 WHERE `id`=@SIS_ENTRY;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@SIS_ENTRY AND `source_type`=0;
+DELETE FROM `smart_scripts` WHERE `entryorguid`=@SIS_ENTRY*100 AND `source_type`=9;
+DELETE FROM `npc_spellclick_spells` WHERE `npc_entry`=@SIS_ENTRY;
+INSERT INTO `npc_spellclick_spells` (`npc_entry`, `spell_id`, `cast_flags`, `user_type`) VALUES
+(@SIS_ENTRY, 93072, 1, 0),
+(@SIS_ENTRY, 93097, 0, 0);
+
+UPDATE `creature_template` SET `scriptname`="npc_blackrock_battle_worg" WHERE `entry`=@BBW_ENTRY;
